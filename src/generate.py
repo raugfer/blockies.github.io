e2c67d2ee3880faee4247d6aee07aca9a93d1d57
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-08-06 18:21:10 -0300

    Adds 1000 json files

---
+++ src/generate.py
@@ -0,0 +1,22 @@
+import json, os, requests, sys
+
+def main():
+    count = int(sys.argv[1]) if len(sys.argv) > 1 else 1000
+    base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    descriptions = []
+    left = count
+    while left > 0:
+        descriptions += requests.get('https://baconipsum.com/api/?type=meat-and-filler&paras=' + str(left)).json()
+        left = count - len(descriptions)
+    for i in range(0, count):
+        index = i + 1
+        item = {
+            'name': 'Blockie #' + str(index),
+            'description': descriptions[i],
+            'image': 'https://picsum.photos/id/' + str(index % 1000) + '/500',
+        }
+        filename = os.path.join(base, 'json', str(index) + '.json')
+        with open(filename, 'w') as outfile:
+            json.dump(item, outfile, separators=(',', ':'))
+
+if __name__ == '__main__': main()
